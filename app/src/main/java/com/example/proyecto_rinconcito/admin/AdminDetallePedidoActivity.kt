2c84b190c12418c69c4a73b4a0b04905990dcc9b
jeliases <j.luisestrada98@gmail.com>
2026-02-08 14:26:23 -0500

    add.configuration.entorno

--- app/src/main/java/com/example/proyecto_rinconcito/admin/AdminDetallePedidoActivity.kt
+++ app/src/main/java/com/example/proyecto_rinconcito/admin/AdminDetallePedidoActivity.kt
@@ -1,16 +1,17 @@
-package com.example.proyecto_rinconcito
+package com.example.proyecto_rinconcito.admin
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyecto_rinconcito.R
 
-class MainActivity : AppCompatActivity() {
+class AdminDetallePedidoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cliente_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/proyecto_rinconcito/cliente/ClienteHomeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.proyecto_rinconcito.cliente
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.proyecto_rinconcito.R
+
+class ClienteHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cliente_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
